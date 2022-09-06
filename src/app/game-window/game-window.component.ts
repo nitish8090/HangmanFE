d8c7a9080a34c67a4f97461ed1a3106c76f28906
Nitish-ROG <nitishpatel1234@gmail.com>
2022-09-07 02:32:53 +0530

    cleaned up code

--- src/app/game-window/game-window.component.ts
+++ src/app/game-window/game-window.component.ts
@@ -1,24 +1,13 @@
-import { AfterViewInit, Component, OnInit, Pipe, PipeTransform } from '@angular/core';
-import { Input } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Game } from '../interfaces/game';
 import { GameService } from '../services/game.service';
-
-@Pipe({
-  name: 'split'
-})
-export class SplitPipe implements PipeTransform {
-
-  transform(value: any, args?: any): any {
-    return value.split('');
-  }
-}
 
 @Component({
   selector: 'app-game-window',
   templateUrl: './game-window.component.html',
   styleUrls: ['./game-window.component.scss']
 })
-export class GameWindowComponent implements OnInit, AfterViewInit {
+export class GameWindowComponent implements OnInit {
 
   game: Game = <Game>{}
   alphabetList: string[] = [];
@@ -27,17 +16,14 @@ export class GameWindowComponent implements OnInit, AfterViewInit {
   constructor(private gameService: GameService) { }
 
   ngOnInit(): void {
-
-   
-
-  }
-
-  ngAfterViewInit(): void {
+    // To create a list of all the characters to make a keyboard
     for (let i = 0; i <= 25; i++) {
       this.alphabetList.push(String.fromCharCode(97 + i))
     }
   }
+
   startNewGame(): void {
+    // Start a new game
     this.gameService.startNewGame().subscribe({
       next: (data) => {
         this.game = data
@@ -48,7 +34,8 @@ export class GameWindowComponent implements OnInit, AfterViewInit {
   }
 
   format_current_pos(): string[] {
-    if (this.game.current_state === 'InProgress'){
+    // Split the string into pieces
+    if (this.game.current_state === 'InProgress') {
       return this.game.current_pos.split("")
     } else {
       return this.game.answer.split("")
@@ -56,11 +43,11 @@ export class GameWindowComponent implements OnInit, AfterViewInit {
   }
 
   makeGuess(c: string): void {
-    console.log(c);
+    // Make a guess by pressing a keyboard key
     this.gameService.makeGuess(this.game.id, c).subscribe({
       next: (data) => {
         this.game = data
-        if (this.game.current_state != 'InProgress'){
+        if (this.game.current_state != 'InProgress') {
           this.isGameOn = false;
         }
       },
